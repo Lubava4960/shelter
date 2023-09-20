079e0957ddeb75717f540dd8ebc5f3cb6b0e156a
Lubava <Lwm28@rambler.ru>
2023-09-20 13:47:13 +0300

    Add answer

--- src/main/java/com/pet/shelter/listener/TelegramBotListener.java
+++ src/main/java/com/pet/shelter/listener/TelegramBotListener.java
@@ -6,11 +6,9 @@ import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.model.request.Keyboard;
-import com.pengrad.telegrambot.request.EditMessageReplyMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
-import org.jetbrains.annotations.NotNull;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,12 +18,16 @@ public class TelegramBotListener implements UpdatesListener {
 
     private final TelegramBot telegramBot;
 
+
     public TelegramBotListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
+
+
     }
 
     @PostConstruct
     private void init() {
+
         telegramBot.setUpdatesListener(this);
     }
 
@@ -33,52 +35,62 @@ public class TelegramBotListener implements UpdatesListener {
     public int process(List<Update> updates) {
         // В этом методе делаем обработку всех событий телеграмма. Например, сделаем ответ на сообщение /start
 
+
         for (Update update : updates) {
             if (update.message() != null && update.message().text() != null) {
                 var text = update.message().text();
-                var chatId = update.message().chat().id();
+                Long chatId = update.message().chat().id();
                 if ("/start".equals(text)) {
-                    SendMessage sendMessage = getSendMessage(chatId);
+                    SendMessage sendMessage = new SendMessage(chatId, """
+                            Привет. Это бот приюта для кошек.
+                            """);
+
+                    InlineKeyboardButton buttonText = new InlineKeyboardButton(" Наш  Лесной приют ");
+                    buttonText.callbackData(" Наш Лесной приют ");
+                    InlineKeyboardButton buttonAddress = new InlineKeyboardButton(" Наш адрес ");
+                    buttonAddress.callbackData(" Наш адрес ");
+                    Keyboard keyboard = new InlineKeyboardMarkup(buttonText, buttonAddress);
+                    sendMessage.replyMarkup(keyboard);
                     telegramBot.execute(sendMessage);
+                } else if (text != null) {
 
-                    //  if ("Наш Лесной приют".equals(text)) {
-               //     telegramBot.execute(new SendMessage(chatId, "В нашем приюте живут кошки , которые скучают и ждут своих хозяев"));
-               // }
-               // if ("Наш адрес".equals(text)) {
-                //    telegramBot.execute(new SendMessage(chatId, "городской округ Истра, деревня Бодрово"));
+                    sendMessage(chatId, "Некорректный формат сообщения!");
+                } else if (update.callbackQuery() != null && update.callbackQuery().data() != null) {
+                    var data = update.callbackQuery().data();
+                    if (data.equals("Наш Лесной приют ")) {
+                        // ответ на кнопку "Наш лесной приют"
 
-                 }
-                //sendMessage.replyMarkup(keyboard);
+                        SendMessage sendMessage = new SendMessage(chatId, """
+                                В нашем приюте живут кошки , которые скучают и ждут своих хозяев
+                                """);
+                        telegramBot.execute(sendMessage);
 
-
+                    } else if (data.equals(" Наш адрес ")) {
+                        // ответ на кнопку "Наш адрес"
+                        SendMessage sendMessage = new SendMessage(chatId, "городской округ Истра, деревня Бодрово" );
+                        telegramBot.execute(sendMessage);
+                        }
+                }
             }
 
+        }
 
 
-}
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    @NotNull
-    private static SendMessage getSendMessage(Long chatId) {
-        SendMessage sendMessage=new SendMessage(chatId, "Добро пожаловать в приют для кошек!");
-        EditMessageReplyMarkup editMessageReplyMarkup= new EditMessageReplyMarkup("1");
-        InlineKeyboardButton buttonText = new InlineKeyboardButton(" Наш  Лесной приют ");
-        buttonText.callbackData(" Наш Лесной приют ");
-        InlineKeyboardButton buttonAddress = new InlineKeyboardButton(" Наш адрес ");
-        buttonAddress.callbackData(" Наш адрес ");
-        Keyboard keyboard = new InlineKeyboardMarkup(buttonText, buttonAddress);
-        sendMessage.replyMarkup(keyboard);
-        return sendMessage;
-    }
+
+
 
     private void sendMessage(Long chatId, String text) {
-       SendMessage sendMessage = new SendMessage(chatId, text);
-        SendResponse sendResponse = telegramBot.execute(sendMessage);
-       // if (!sendResponse.isOk()) {
-           // logger.error("Произошла ошибка!!! {}",sendResponse.description());
-        }
+        SendMessage sendMessage = new SendMessage(chatId, text);
+}
     }
+
+
+
+
+
 
 
 
